2bf85feab3b4d51545ed608c0cc9984ed137e18c
Jait Dixit <jait.dixit@tum.de>
2016-03-12 08:02:16 +0100

    Update server
    - Removed code which used elasticsearch to find popular searches
    - Add 'source' to search results

--- backend/server/elastic.py
+++ backend/server/elastic.py
@@ -3,13 +3,6 @@ from elasticsearch_dsl.connections import connections
 from elasticsearch_dsl import DocType, String, Integer
 
 
-class Query(DocType):
-    class Meta:
-        index = 'queries'
-    query_string = String()
-    count = Integer()
-
-
 class ElasticStorage:
     _instance = None
 
@@ -25,35 +18,6 @@ class ElasticStorage:
             else:
                 cls._instance = cls(url='http://134.168.38.217:9200')
         return cls._instance
-
-    def store_query(self, query_string):
-        query_string = query_string.lower()
-        # check if the query string exists
-        client = connections.get_connection()
-        search = Search(using=client, index='queries') \
-                 .query("match", query_string=query_string)
-        search.execute()
-        if search:
-            # if found increment the count
-            search.count += 1
-        else:
-            # create a query object
-            es_query = Query(
-                query_string=query_string,
-                count=1
-            )
-            es_query.save()
-
-    def get_top_queries(self):
-        client = connections.get_connection()
-        search = Search(using=client, index='queries') \
-                 .sort('-count')
-        search = search[1:10]
-        for hit in search:
-            yield {
-                'query': hit.query_string
-            }
-
 
     def query_articles(self, query):
         client = connections.get_connection()
--- backend/server/server.py
+++ backend/server/server.py
@@ -6,22 +6,46 @@ from elastic import ElasticStorage
 app = Flask(__name__)
 CORS(app)
 
-def
+source_map = {
+    'cnn.com': 'CNN', 'nytimes.com': 'New York Times',
+    'huffingtonpost.com': 'The Huffington Post', 'theguardian.com': 'The Guardian',
+    'foxnews.com': 'Fox News', 'forbes.com': 'Forbes',
+    'timesofindia.indiatimes.com': 'The Times of India', 'bbc.co.uk': 'BBC',
+    'usatoday.com': 'USA Today', 'bloomberg.com': 'Bloomberg',
+    'wsj.com': 'The Wall Street Journal', 'reuters.com': 'Reuters',
+    'nbcnews.com': 'NBC News', 'money.cnn.com': 'CNN Money',
+    'indianexpress.com': 'The Indian Express', 'cbsnews.com': 'CBS News',
+    'abcnews.go.com': 'ABC News', 'latimes.com': 'LA Times',
+    'time.com': 'Time', 'nypost.com': 'NY Post', 'cnbc.com': 'CNBC',
+    'thehindu.com': 'The Hindu', 'chron.com': 'CHRON',
+    'theatlantic.com': 'The Atlantic', 'breitbart.com': 'Breitbart',
+    'sfgate.com': 'SF Gate', 'usnews.com': 'US News',
+    'hindustantimes.com': 'Hindustan Times', 'hollywoodreporter.com': 'The Hollywood Reporter',
+    'fortune.com': 'Fortune', 'chicagotribune.com': 'Chicago Tribune',
+    'news.com.au': 'news.com.au'
+}
 
 
 @app.route('/search')
 def search():
     query = request.args.get('q', '')
-    client = ElasticStorage.get_instance()
+    client = ElasticStorage.get_instance(dev=False)
     articles = client.query_articles(query)
+    articles = list(articles)
+    for article in articles:
+        for key, value in source_map.items():
+            if key in article['url']:
+                article['source'] = value
     return jsonify(
-        articles=list(articles)
+        articles=articles
     )
 
-@app.route('/top')
-def top():
-    client = ElasticStorage.get_instance()
-    queries = client.get
+
+@app.route('/popular')
+def popular():
+    pass
+
+
 if __name__ == "__main__":
     port = os.getenv('VCAP_APP_PORT', '5000')
-    app.run(host='0.0.0.0', port=int(port))
+    app.run(host='0.0.0.0', port=int(port), debug=True)
